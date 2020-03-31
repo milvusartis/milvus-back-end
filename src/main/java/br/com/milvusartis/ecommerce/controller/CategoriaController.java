df70c3ac19f8a67a70df0025900fbbdae89c55f2
Treinamento <Treinamento@RD723894.raiadrogasil.com>
2020-03-31 13:23:29 -0300

    controller

--- src/main/java/br/com/milvusartis/ecommerce/controller/CategoriaController.java
+++ src/main/java/br/com/milvusartis/ecommerce/controller/CategoriaController.java
@@ -1,6 +1,6 @@
 package br.com.milvusartis.ecommerce.controller;
 
-        import br.com.milvusartis.ecommerce.exception.ResourceNotFoundException;
+import br.com.milvusartis.ecommerce.exception.ResourceNotFoundException;
         import br.com.milvusartis.ecommerce.model.bo.CategoriaBO;
         import br.com.milvusartis.ecommerce.model.dto.CategoriaDTO;
         import br.com.milvusartis.ecommerce.model.dto.ClienteDTO;
@@ -16,59 +16,72 @@ package br.com.milvusartis.ecommerce.controller;
         import java.util.List;
         import java.util.Optional;
 
-
 @RestController
 public class CategoriaController {
 
     @Autowired
-    CategoriaRepository repository;
-
+    CategoriaRepository categoriaRepository;
 
     @Autowired
     CategoriaBO categoriaBO;
 
-
-    @GetMapping("/categorias")
-    public ResponseEntity<?> listar() {
-        List<Categoria> ListaCategorias = repository.findAll();
-        List<CategoriaDTO> listaDeCategoriasResposta = new ArrayList<>();
-
-        ListaCategorias.forEach((categoria) -> {
-            listaDeCategoriasResposta.add(categoriaBO.parseToDTO(categoria));
-        });
-        return ResponseEntity.status(HttpStatus.OK).body(listaDeCategoriasResposta);
-    }
-
-
-
     @PostMapping("/categorias")
     public ResponseEntity<?> cadastrar(@RequestBody CategoriaDTO categoriaDTO) {
+
         if (categoriaDTO == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Categoria não pode estar vazia");
         }
 
-        Categoria categoriaEntity = repository.save(categoriaBO.parseToPOJO(categoriaDTO));
+        Categoria categoriaEntity = categoriaRepository.save(categoriaBO.parseToPOJO(categoriaDTO));
+
         return ResponseEntity.status(HttpStatus.CREATED).body(categoriaBO.parseToDTO(categoriaEntity));
+
+    }
+
+    @GetMapping("/categorias")
+    public ResponseEntity<?> listar() {
+
+        List<Categoria> listaCategorias = categoriaRepository.findAll();
+        List<CategoriaDTO> listaDeCategoriasResposta = new ArrayList<>();
+
+        listaCategorias.forEach((categoria) -> {
+            listaDeCategoriasResposta.add(categoriaBO.parseToDTO(categoria));
+        });
+
+        return ResponseEntity.status(HttpStatus.OK).body(listaDeCategoriasResposta);
 
     }
 
     @GetMapping("/categorias/{id}")
     public ResponseEntity<?> mostrar(@PathVariable("id") Long id) {
-        Optional<Categoria> opt_categoria = repository.findById(id);
+
+        Optional<Categoria> opt_categoria = categoriaRepository.findById(id);
         Categoria categoria = opt_categoria.orElseThrow(() -> new ResourceNotFoundException("Categoria não encontrada"));
+
         return ResponseEntity.status(HttpStatus.OK).body(categoriaBO.parseToDTO(categoria));
+
     }
 
+//    @GetMapping("/categorias/{nm}")
+//    public ResponseEntity<?> mostrar(@PathVariable("nm") String nm) {
+//
+//        Optional<Categoria> opt_categoria = categoriaRepository.findByNome(nm);
+//        Categoria categoria = opt_categoria.orElseThrow(() -> new ResourceNotFoundException("Categoria não encontrada"));
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(categoriaBO.parseToDTO(categoria));
+//
+//    }
 
     @DeleteMapping("/categorias/{id}")
     public ResponseEntity<?> remover(@PathVariable("id") Long id) {
-        return repository.findById(id)
+
+        return categoriaRepository.findById(id)
                 .map(categoria -> {
-                    repository.delete(categoria);
-                    return ResponseEntity.status(HttpStatus.OK).body("Categoria excluida");
+                    categoriaRepository.delete(categoria);
+                    return ResponseEntity.status(HttpStatus.OK).body("Categoria excluída");
 
                 }).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }
 
+    }
 
 }
--- src/main/java/br/com/milvusartis/ecommerce/controller/ClienteController.java
+++ src/main/java/br/com/milvusartis/ecommerce/controller/ClienteController.java
@@ -1,78 +1,75 @@
-package br.com.milvusartis.ecommerce.controller;
-
-import br.com.milvusartis.ecommerce.exception.ResourceNotFoundException;
-import br.com.milvusartis.ecommerce.model.bo.ClienteBO;
-import br.com.milvusartis.ecommerce.model.bo.ClienteResponseBO;
-import br.com.milvusartis.ecommerce.model.dto.ClienteDTO;
-import br.com.milvusartis.ecommerce.model.dto.ClienteResponseDTO;
-import br.com.milvusartis.ecommerce.model.entity.Cliente;
-import br.com.milvusartis.ecommerce.repository.ClienteRepository;
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Optional;
-
-@RestController
-@RequiredArgsConstructor
-public class ClienteController {
-
-    @Autowired
-    ClienteRepository repository;
-
-
-    @Autowired
-    private ClienteBO clienteBO;
-
-    @Autowired
-    private ClienteResponseBO clienteResponseBO;
-
-
-    @GetMapping("/clientes")
-    public ResponseEntity<?> listar() {
-        List<Cliente> clientes = repository.findAll();
-        List<ClienteResponseDTO> listaDeClientesResposta = new ArrayList<>();
-
-        clientes.forEach((cliente) -> {
-            listaDeClientesResposta.add(clienteResponseBO.parseToDTO(cliente));
-        });
-        return ResponseEntity.status(HttpStatus.OK).body(listaDeClientesResposta);
-    }
-
-
-
-    @PostMapping("/clientes")
-    public ResponseEntity<?> cadastrar(@RequestBody ClienteDTO clienteDTO) {
-        if (clienteDTO == null) {
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não pode estar vazio");
-        }
-
-        Cliente clienteEntity = repository.save(clienteBO.parseToPOJO(clienteDTO));
-        return ResponseEntity.status(HttpStatus.CREATED).body(clienteBO.parseToDTO(clienteEntity));
-
-    }
-
-    @GetMapping("/clientes/{id}")
-    public ResponseEntity<?> mostrar(@PathVariable("id") Long id) {
-        Optional<Cliente> opt_cliente = repository.findById(id);
-        Cliente cliente = opt_cliente.orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrado"));
-        return ResponseEntity.status(HttpStatus.OK).body(clienteBO.parseToDTO(cliente));
-    }
-
-
-    @DeleteMapping("/clientes/{id}")
-    public ResponseEntity<?> remover(@PathVariable("id") Long id) {
-        return repository.findById(id)
-                .map(cliente -> {
-                    repository.delete(cliente);
-                    return ResponseEntity.status(HttpStatus.OK).body("Cliente excluido");
-
-                }).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
-    }
-
-
-}
+//package br.com.milvusartis.ecommerce.controller;
+//
+//import br.com.milvusartis.ecommerce.exception.ResourceNotFoundException;
+//import br.com.milvusartis.ecommerce.model.bo.ClienteBO;
+//import br.com.milvusartis.ecommerce.model.dto.ClienteDTO;;
+//import br.com.milvusartis.ecommerce.model.entity.Cliente;
+//import br.com.milvusartis.ecommerce.repository.ClienteRepository;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.HttpStatus;
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//import java.util.Optional;
+//
+//@RestController
+//public class ClienteController {
+//
+//    @Autowired
+//    ClienteRepository clienteRepository;
+//
+//    @Autowired
+//    ClienteBO clienteBO;
+//
+//    @PostMapping("/clientes")
+//    public ResponseEntity<?> cadastrar(@RequestBody ClienteDTO clienteDTO) {
+//
+//        if (clienteDTO == null) {
+//            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Cliente não pode estar vazio");
+//        }
+//
+//        Cliente clienteEntity = clienteRepository.save(clienteBO.parseToPOJO(clienteDTO));
+//
+//        return ResponseEntity.status(HttpStatus.CREATED).body(clienteBO.parseToDTO(clienteEntity));
+//
+//    }
+//
+//    @GetMapping("/clientes")
+//    public ResponseEntity<?> listar() {
+//
+//        List<Cliente> listaCliente = clienteRepository.findAll();
+//        List<ClienteDTO> listaDeClienteResposta = new ArrayList<>();
+//
+//        listaCliente.forEach((cliente) -> {
+//            listaDeClienteResposta.add(clienteBO.parseToDTO(cliente));
+//        });
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(listaDeClienteResposta);
+//
+//    }
+//
+//    @GetMapping("/clientes/{id}")
+//    public ResponseEntity<?> mostrar(@PathVariable("id") Long id) {
+//
+//        Optional<Cliente> opt_cliente = clienteRepository.findById(id);
+//        Cliente cliente = opt_cliente.orElseThrow(() -> new ResourceNotFoundException("Cliente não encontrad0"));
+//
+//        return ResponseEntity.status(HttpStatus.OK).body(clienteBO.parseToDTO(cliente));
+//
+//    }
+//
+//    @DeleteMapping("/clientes/{id}")
+//    public ResponseEntity<?> remover(@PathVariable("id") Long id) {
+//
+//        return clienteRepository.findById(id)
+//                .map(cliente -> {
+//                    clienteRepository.delete(cliente);
+//                    return ResponseEntity.status(HttpStatus.OK).body("Cliente excluído");
+//
+//                }).orElse(ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+//
+//    }
+//
+//}
