36bcd1aeb5209f05da8e80bf1f0e583dd883c569
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-04-09 14:19:04 -0300

    Configuração do Spring Security permitindo métodos get

--- src/main/java/br/com/milvusartis/ecommerce/config/WebSecurityConfig.java
+++ src/main/java/br/com/milvusartis/ecommerce/config/WebSecurityConfig.java
@@ -2,15 +2,23 @@ package br.com.milvusartis.ecommerce.config;
 
 import br.com.milvusartis.ecommerce.service.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
 
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
@@ -25,27 +33,59 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
 
+//    @Override
+//    public void configure(HttpSecurity http) throws Exception {
+//        http
+//                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
+//                .and()
+//                .csrf().disable()
+//                .httpBasic()
+//                .and()
+//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//                .and()
+//
+//                .authorizeRequests()
+//
+//                //CONFIGURAÇÃO DE REQUEST
+//                .anyRequest().permitAll()
+//                .and().headers().frameOptions()
+//                //Disable para desbloquar o formulário do H2
+//                .disable();
+//    }
+
+    @Autowired
+    private Environment env;
+
+    private static final String[] PUBLIC_MATCHERS = {
+            "/h2/**",
+            "/h2-console/**",
+    };
+
+    private static final String[] PUBLIC_MATCHERS_GET = {
+            "/produtos/**",
+            "/categorias/**",
+    };
+
     @Override
-    public void configure(HttpSecurity http) throws Exception {
-        http
-                .cors().configurationSource(request -> new CorsConfiguration().applyPermitDefaultValues())
-                .and()
-                .csrf().disable()
-                .httpBasic()
-                .and()
-                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
+    protected void configure(HttpSecurity http) throws Exception {
 
-                .authorizeRequests()
+        if(Arrays.asList((env.getActiveProfiles())).contains("test") || Arrays.asList((env.getActiveProfiles())).contains("dev")){
+            http.headers().frameOptions().disable();
+        }
 
-                //CONFIGURAÇÃO DE REQUEST
-                .anyRequest().permitAll()
-                .and().headers().frameOptions()
-                //Disable para desbloquar o formulário do H2
-                .disable();
-
-
+        http.cors().and().csrf().disable();
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, PUBLIC_MATCHERS_GET).permitAll()
+                .antMatchers(PUBLIC_MATCHERS).permitAll()
+                .anyRequest().authenticated();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
 }
