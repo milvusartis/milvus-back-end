230263c101dda12f7a7abb9dee469de1e22f189f
Diógenes Bezerra Pereira <ads.diogenes@gmail.com>
2020-04-12 20:14:17 -0300

    Refresh token

--- src/main/java/br/com/milvusartis/ecommerce/controller/AuthController.java
+++ src/main/java/br/com/milvusartis/ecommerce/controller/AuthController.java
@@ -2,19 +2,29 @@ package br.com.milvusartis.ecommerce.controller;
 
 import br.com.milvusartis.ecommerce.model.entity.Usuario;
 import br.com.milvusartis.ecommerce.repository.UsuarioRepository;
+import br.com.milvusartis.ecommerce.security.JWTUtil;
+import br.com.milvusartis.ecommerce.security.UserSS;
+import br.com.milvusartis.ecommerce.service.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
+@RequestMapping(value = "/auth")
 public class AuthController {
 
     @Autowired
     private UsuarioRepository usuarioRepository;
 
+    @Autowired
+    private JWTUtil jwtUtil;
 
     @GetMapping("/auth/token")
     public ResponseEntity<?> perfil(Authentication authentication) {
@@ -32,5 +42,14 @@ public class AuthController {
 //            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Usuário sem permissão");
 //    }
 
+
+    @RequestMapping(value = "/refresh_token", method = RequestMethod.POST)
+    public ResponseEntity<Void> refreshToken(HttpServletResponse response) {
+        UserSS user = UsuarioService.authenticated();
+        String token = jwtUtil.generateToken(user.getUsername());
+        response.addHeader("Authorization", "Bearer " + token);
+        response.addHeader("access-control-expose-headers", "Authorization");
+        return ResponseEntity.noContent().build();
+    }
 
 }
